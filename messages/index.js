87cec224809b554897995b266752e80c6882dc6b
Felix Moalusi <felixm@thoughtworks.com>
2017-02-08 22:31:18 +0200

    changes to the default dialog

--- messages/index.js
+++ messages/index.js
@@ -13,9 +13,63 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
 
 var bot = new builder.UniversalBot(connector);
 
-bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
-});
+bot.dialog('/', [
+    function (session) {
+        session.userData.playerOne = {'name': 'Awesome', 'score': 0};   
+        session.userData.playerTwo = {'name': 'RoxStar', 'score': 0};  
+        session.beginDialog('score', session.userData);//session.userData.profile
+    }
+]);
+
+bot.dialog('startGame', [
+    function (session, args, next) { 
+        session.userData.profile = args || {};
+
+        if (!session.userData.profile.name) {
+            builder.Prompts.text(session, "Hi!");
+        } else {
+            next();
+        }
+    },
+    function (session, results, next) {
+        if (results.response === 'start') {
+            builder.Prompts.text(session, "A new game between: " + session.userData.playerOne.name + ', and ' + session.userData.playerTwo.name);
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        if (results.response === 'player one scores') {
+            session.userData.playerOne.score++;
+            builder.Prompts.text(session, "Score: " + session.userData.playerOne.score + ', and ' + session.userData.playerTwo.name);
+        }
+    }
+]);
+
+bot.dialog('score', [
+    function (session) {
+        builder.Prompts.text(session, session.userData.playerOne.name + ': ' + session.userData.playerOne.score + ', and ' +  session.userData.playerTwo.name + ': ' + session.userData.playerTwo.score);        
+    },
+    function (session, results) {
+        if (results.response === session.userData.playerOne.name + ' scores')
+        {
+            session.userData.playerOne.score++;
+        }
+        else if (results.response === session.userData.playerTwo.name + ' scores') {
+            session.userData.playerTwo.score++;
+        }
+        session.replaceDialog('score', session.dialogData);
+    }
+]);
+
+bot.dialog('gameOver', [
+    function (session) {
+        builder.Prompts.text(session, 'Player wins! By ' + session.dialogData.playerOne + ' to ' + session.dialogData.playerTwo);
+    },
+    function (session, results) {
+        session.endDialog();
+    }
+]);
 
 if (useEmulator) {
     var restify = require('restify');
